c82bd633b782f3f8121e43f1ad310e47730059d4
Petya Kuzmanova <p.bozidarova@gmail.com>
2021-04-22 00:31:30 +0300

    Reverse integer

--- javaBrains/ReverseInteger.java
+++ javaBrains/ReverseInteger.java
@@ -4,12 +4,19 @@ public class ReverseInteger {
 
     public static void main(String[] args) {
         int number = 54321;
-        int reversed = 0;
 
+        System.out.println(reverse(number));
+    }
+
+    private static int reverse(int number){
+        int reversed = 0;
         while ( number > 0 ){
             reversed = reversed * 10 + number % 10;
-            number = number / 10;
+            number /= 10;
+            if(reversed > Integer.MAX_VALUE || reversed < Integer.MIN_VALUE){
+                return 0;
+            }
         }
-        System.out.println(reversed);
+        return reversed;
     }
 }
