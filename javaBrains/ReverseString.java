897a388599e65673c98b267e04b5b427b81c8251
Petya Kuzmanova <p.bozidarova@gmail.com>
2021-04-21 18:34:39 +0300

    initial commit

---
+++ javaBrains/ReverseString.java
@@ -0,0 +1,27 @@
+package javaBrains;
+
+public class ReverseString {
+    public static void main(String[] args) {
+        String str = "Hello World!";
+        System.out.println(reverseWithStringBuilder(str));
+        System.out.println(reverseManually(str));
+    }
+
+    private static String reverseWithStringBuilder(String str) {
+        return new StringBuilder(str)
+                .reverse()
+                .toString();
+    }
+
+    private static String reverseManually(String str) {
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = str.length() - 1; i >= 0 ; i--) {
+            sb.append(str.charAt(i));
+        }
+
+        return sb.toString();
+    }
+
+
+}
