2867b7ad5a55e1b3b815b42aeb859688a67a642d
Petya Kuzmanova <p.bozidarova@gmail.com>
2021-04-22 16:20:46 +0300

    Sample input

--- hackerRank/PileOfSocks.java
+++ hackerRank/PileOfSocks.java
@@ -1,6 +1,9 @@
 package hackerRank;
 //There is a large pile of socks that must be paired by color.
 // Given an array of integers representing the color of each sock, determine how many pairs of socks with matching colors there are.
+//Sample Input
+//        9
+//        10 20 20 10 10 30 50 10 20
 
 import java.io.BufferedWriter;
 import java.io.FileWriter;
