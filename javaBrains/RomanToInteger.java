cf2a80efed9e58eec0fc3bda1efacbfb50cffa5c
Petya Kuzmanova <p.bozidarova@gmail.com>
2021-04-22 18:54:54 +0300

    Roman numbers

--- javaBrains/RomanToInteger.java
+++ javaBrains/RomanToInteger.java
@@ -2,4 +2,23 @@ package javaBrains;
 
 public class RomanToInteger {
 
+    public static void main(String[] args) {
+        System.out.println(intToRoman(104));
+    }
+
+    public static String intToRoman(int num){
+        String romanNumber = "";
+
+        String[] thousands = {"", "M", "MM", "MMM"};
+        String[] hundreds = {"", "C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM"};
+        String[] tens = {"", "X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC"};
+        String[] units = {"", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"};
+
+        romanNumber = thousands[num / 1000]
+                + hundreds[(num % 1000)/100]
+                + tens[(num % 100) / 10]
+                + units[(num % 10)];
+
+        return romanNumber;
+    }
 }
