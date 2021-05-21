741eea16b65da978862804ee157e8b5b7b5cea93
Petya Kuzmanova <p.bozidarova@gmail.com>
2021-05-21 18:44:27 +0300

    Day 26 and 27

--- hackerRank_30DaysOfCode/Day26.java
+++ hackerRank_30DaysOfCode/Day26.java
@@ -1,4 +1,46 @@
 package hackerRank_30DaysOfCode;
 
+import java.time.LocalDate;
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class Day26 {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int[] returnedDateArray = Arrays.stream(scanner.nextLine().split("\\s+"))
+                .mapToInt(Integer::parseInt).toArray();
+        int[] dueDateArray = Arrays.stream(scanner.nextLine().split("\\s+"))
+                .mapToInt(Integer::parseInt).toArray();
+
+
+        LocalDate returnedDate = LocalDate.of(
+                returnedDateArray[2],
+                returnedDateArray[1],
+                returnedDateArray[0]);
+
+        LocalDate dueDate = LocalDate.of(
+                dueDateArray[2],
+                dueDateArray[1],
+                dueDateArray[0]);
+
+        long fine = 0;
+
+        if(returnedDate.isAfter(dueDate)){
+            if(returnedDate.getYear() == dueDate.getYear()){
+                if(returnedDate.getMonthValue() == dueDate.getMonthValue()){
+                    fine = 15 * ( returnedDate.getDayOfMonth() - dueDate.getDayOfMonth());
+                } else{
+                    fine = 500 * ( returnedDate.getMonthValue() - dueDate.getMonthValue());
+                }
+            }else {
+                fine = 10000;
+            }
+
+        }
+
+        System.out.println(fine);
+    }
+
 }
