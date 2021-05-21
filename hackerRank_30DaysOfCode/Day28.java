ff6e6e2ed2555f367fc330f1bda2bda1b458fdc5
Petya Kuzmanova <p.bozidarova@gmail.com>
2021-05-21 19:05:00 +0300

    Day 28 & 29

--- hackerRank_30DaysOfCode/Day28.java
+++ hackerRank_30DaysOfCode/Day28.java
@@ -1,4 +1,48 @@
 package hackerRank_30DaysOfCode;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.stream.IntStream;
+
 public class Day28 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(bufferedReader.readLine().trim());
+
+        String emailRegEx = ".+@gmail\\.com$";
+        Pattern pattern = Pattern.compile(emailRegEx);
+        List<String> list = new ArrayList();
+
+        IntStream.range(0, N).forEach(NItr -> {
+            try {
+                String[] firstMultipleInput = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
+
+                String firstName = firstMultipleInput[0];
+
+                String emailID = firstMultipleInput[1];
+
+                Matcher matcher = pattern.matcher(emailID);
+                if (matcher.find()){
+                    list.add(firstName);
+                }
+
+            } catch (IOException ex) {
+                throw new RuntimeException(ex);
+            }
+        });
+        Collections.sort(list);
+        for (String name : list){
+            System.out.println(name);
+        }
+
+        bufferedReader.close();
+    }
 }
--- hackerRank_30DaysOfCode/Day29.java
+++ hackerRank_30DaysOfCode/Day29.java
@@ -1,4 +1,64 @@
 package hackerRank_30DaysOfCode;
 
+import java.io.*;
+import java.util.stream.IntStream;
+
 public class Day29 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+//        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int t = Integer.parseInt(bufferedReader.readLine().trim());
+
+        IntStream.range(0, t).forEach(tItr -> {
+            try {
+                String[] firstMultipleInput = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
+
+                int count = Integer.parseInt(firstMultipleInput[0]);
+
+                int lim = Integer.parseInt(firstMultipleInput[1]);
+
+                int res = Result.bitwiseAnd(count, lim);
+                System.out.println(res);
+//                bufferedWriter.write(String.valueOf(res));
+//                bufferedWriter.newLine();
+            } catch (IOException ex) {
+                throw new RuntimeException(ex);
+            }
+        });
+
+        bufferedReader.close();
+//        bufferedWriter.close();
+    }
+
+}
+
+
+class Result {
+
+    /*
+     * Complete the 'bitwiseAnd' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts following parameters:
+     *  1. INTEGER N
+     *  2. INTEGER K
+     */
+
+    public static int bitwiseAnd(int N, int K) {
+        int finalResult = 0;
+
+        for(int i = 1; i < N ; i++){
+            for(int j = i+1; j <= N ; j++){
+                int amp = i&j;
+                if(amp < K && amp > finalResult)
+                    finalResult = amp;
+            }
+        }
+
+        return finalResult;
+    }
+
+
 }
